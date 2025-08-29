5ee73522abd35ea7a573b58b34c6b90559f77c08
qrim128 <markkennethmarimla28@gmail.com>
2025-08-29 15:26:30 +0800

    index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ document.addEventListener("keydown", function (ev) {
 
     // ✅ Hit window unchanged
     if (key_pressed === element.dataset.answer.toString()) {
-        if (top < -475 && top > -500) {
+        if (top < -475 && top > -505) {
             streak++;
             element.style.color = "lime"; // mark correct
             document.querySelector('.streak').innerHTML = streak;
